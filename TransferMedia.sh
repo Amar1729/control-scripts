8b54623fb4b857ff95bf8970f102d436fabeda76
Amar1729 <amartiger20@gmail.com>
2016-01-03 00:19:20 -0500

    Finalized transfer script ?

--- TransferMedia.sh
+++ TransferMedia.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
-#$DEVS = # arg1
-#$FILES = # arg2
-
 while [[ $# > 0 ]]
 do
 key="$1"
 
 case $key in
-	-a|--approved)
-	TEST=false
-	echo "Approved to rename ..."
-	echo ""
+	-d|--disk)
+	DEVS="$2"
 	shift
 	;;
-	-p|--path)
-	path="$2"
-#		echo "this is"
-#	ls "$path"
-#		echo "how we do it with"
-#		echo "$path"
-#	path="../testnames/Season 1/"
-#		echo "again now"
-#		ls "$path"
-#		echo "end test"
-	filenum=1
+	-f|--files)
+	FILES="$2"
 	shift
 	;;
-#	-s|--sub)
-#	sub=true
-#	shift
-#	;;
 	*)
 	echo wut	#unknown option
 	;;
@@ -38,7 +20,5 @@ esac
 shift
 done
 
-
-
-sudo mount $DEVS /media/USB_64
+sudo mount -o gid=pi,uid=pi $DEVS /media/USB_64
 sudo cp -r $FILES /media/USB_64/Movies
