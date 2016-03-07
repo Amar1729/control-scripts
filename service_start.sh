e591048426269a83fce9349fc61398161a007de7
Amar1729 <amartiger20@gmail.com>
2016-03-06 23:33:55 -0500

    Modified services; added backup

--- service_start.sh
+++ service_start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Script created by Amar Paul:	Oct 25, 2015
 # 	I need certain services to run from startup
-#	Those services are (as of 10/28/15):
+#	Those services are (as of 03/06/15):
 #  deluged	deluged
 #  deluge-web	deluge-web
-#  noip2	sudo /usr/local/bin/noip2
-#  minidlna	sudo service minidlna start
+#  noip2	sudo service noip2 start
+#  btsync	sudo service btsync start
+#  minidlna	sudo service minidlna start	be careful (check IP)
 #		sudo service minidlna -R
 #		sudo service minidlna force-reload
-#  netatalk	sudo /etc/init.d/netatalk start
-#  avahi-daemon	sudo /etc/init.d/avahi-daemon start
+#  netatalk	sudo /etc/init.d/netatalk start		don't need
+#  avahi-daemon	sudo /etc/init.d/avahi-daemon start	don't need
 
 # apache stuff is kept in 	/var/www/html
 #				and http.conf where?
